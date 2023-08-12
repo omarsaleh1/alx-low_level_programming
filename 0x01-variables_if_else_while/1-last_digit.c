18dd1e69e0933081f4cb2711fba2ba376c433ebf
omar <business.omarsaleh@gmail.com>
2023-08-12 07:56:27 -0700

    Finish the second Task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,38 +4,37 @@
 /* more headers goes there */
 
 /**
- * main - Entry point
- *
- * Description: This program generates a random number, computes its last digit,
- * and prints information about the last digit.
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: This program generates a random number, computes its last digit,
+* and prints information about the last digit.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int n;
-    int lastdigit;  /* Declare the variable at the beginning of the block */
+int n;
+int lastdigit;  /* Declare the variable at the beginning of the block */
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    lastdigit = n % 10;  /* Initialize the variable */
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastdigit = n % 10;  /* Initialize the variable */
 
-    printf("Last digit of %d is %d ", n, lastdigit);
+printf("Last digit of %d is %d ", n, lastdigit);
 
-    if (lastdigit > 5)
-    {
-        printf("and is greater than 5\n");
-    }
-    else if (lastdigit == 0)
-    {
-        printf("and is 0\n");
-    }
-    else
-    {
-        printf("and is less than 6 and not 0\n");
-    }
-
-    /* your code goes there */
-    return (0);
+if (lastdigit > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (lastdigit == 0)
+{
+printf("and is 0\n");
+}
+else
+{
+printf("and is less than 6 and not 0\n");
 }
 
+/* your code goes there */
+return (0);
+}
