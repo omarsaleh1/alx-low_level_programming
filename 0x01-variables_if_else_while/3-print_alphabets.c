8a794413d5b687187a2b2661a0d1d257bbd863dd
omar <business.omarsaleh@gmail.com>
2023-08-12 08:45:13 -0700

    Finish the Third Task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 /* More Headers goes here */
+
 /**
-* (Main) Enty Point *
-* Description : This program is print the alphabet in lowercase then in uppercase *
-* Return Always 0 Success *
-**/
-int main() {
-char letter;
-char LETTER;
-letter = 'a';
-LETTER = 'A';
-while (letter <= 'z') {
-putchar(letter);
-letter++;
-}
-while (LETTER <= 'Z') {
-putchar(LETTER);
-LETTER++;
-}
-putchar('\n');
-return 0;
+ * main - Entry Point
+ *
+ * Description: This program prints the alphabet in lowercase
+ *              and then in uppercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter;
+    char LETTER;
+
+    letter = 'a';
+    LETTER = 'A';
+
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    while (LETTER <= 'Z')
+    {
+        putchar(LETTER);
+        LETTER++;
+    }
+
+    putchar('\n');
+
+    return (0);
 }
